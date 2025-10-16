2df09ccd48b77341bdf2e4da1ce6ee65a94f2e41
andrewbitlab <andrew.bitlab@gmail.com>
2025-10-16 15:13:08 +0200

    Increase logo size to 48px for better visibility

--- components/brand/Logo.tsx
+++ components/brand/Logo.tsx
@@ -12,13 +12,13 @@ export function Logo({ className, showText = true }: LogoProps) {
   return (
     <div className={cn('flex items-center', className)}>
       {/* Complete logo with icon + text (logo-name.png contains both) */}
-      <div className="relative h-10">
+      <div className="relative h-12">
         <Image
           src="/brand/logo-name.png"
           alt="DEUS QUANT"
-          width={320}
-          height={40}
-          className="h-full w-auto object-contain"
+          width={384}
+          height={48}
+          className="h-full w-auto object-contain m-0 p-0"
           priority
         />
       </div>
